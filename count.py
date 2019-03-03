0ba1479ea00d53edcf4be087d8491937724bd0f4
Danting Zhang <zdt19970923@gmail.com>
2019-03-04 00:31:18 +1100

    implement

--- count.py
+++ count.py
@@ -3,11 +3,27 @@
 def count_char(text):
     # TODO count the number of times each character occurs in the text
     # and print out each character along with its count
-    pass
+   master = {}
+   for item  in text:
+     if item in master:
+        master[item] += 1
+     else:
+        master[item] = 1
+   for key,val in master.items():
+     print(key,val)
 
 def count_char_insensitive(text):
     # TODO do the same as `count_char` but in a case-insensitive manner
-    pass
+
+   master = {}
+   for item  in text:
+     item = item.lower()
+     if item in master:
+        master[item] += 1
+     else:
+        master[item] = 1
+   for key,val in master.items():
+     print(key,val)
 
 
 def count_char_ordered(text):
@@ -16,4 +32,15 @@ def count_char_ordered(text):
     
     # This task is quite difficult, so please feel free to make use of
     # resources online (Python docs, Stack Overflow, etc.)
-    pass
+    
+   master = {}
+   for item  in text:
+     item = item.lower()
+     if item in master:
+        master[item] += 1
+     else:
+        master[item] = 1
+   master = sorted(master.items(),key=lambda x:x[1],reverse=True)
+   for i in range(len(master)):
+        print(master[i][0],master[i][1])
+
--- number_list.py
+++ number_list.py
@@ -1,35 +1,47 @@
 
 def find_reverse(numbers):
     #TODO: find the reverse of the list
-    pass
-
+    return numbers[::-1]
 def find_max(numbers):
     #TODO: find the maximum of the list
-    pass
+    return max(numbers)
 
 def find_min(numbers):
     #TODO: find the minimum of the list
-    pass
+    return min(numbers)
 
 def find_sum(numbers):
     #TODO: find the sum of all the numbers in the list
-    pass
+    return sum(numbers)
 
 def find_average(numbers):
     #TODO: find the average of all the numbers in the list
-    pass
+    return sum(numbers)/len(numbers)
 
 def find_descending(numbers):
     #TODO: numbers sorted in descending order
-    pass
 
+    return sorted(numbers,reverse = True)
 def second_smallest(numbers):
     #TODO: find the second smallest
-    pass
+    min_1 = 10000
+    min_2 = 10000
+    master = set(numbers)
+
+    for num in master:
+       if num <= min_1:
+          min_2 = min_1
+          min_1 = num
+       elif num < min_2:
+          min_2 = num
+    return min_2
 
 def kth_smallest(numbers, k):
     #TODO: find the kth smallest number in the list
-    pass
+    
+    number = set(numbers)
+    number = sorted(number)
+    return number[k-1]
 
 if __name__ == '__main__':
     # If you are testing, place your print statements here
